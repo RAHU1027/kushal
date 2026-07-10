fe41d1327806e8e5ed28c98a656eb5e2c09905dc
RAHU1027 <sanjitrai88888888@gmail.com>
2026-07-10 11:29:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,8 @@ import os
 import time
 
 # --- CONFIG ---
-API_TOKEN = '8650620955:AAGbeAnHtxhYqaGwG9XZaEhkTP7nMLds1cA'
-bot = telebot.TeleBot(API_TOKEN)
+API_TOKEN = '5b108bd2fdd31c0c34bc65f24a5216a0'
+bot = telebot.TeleBot(8410119226:AAEDaMjNEmPINLbJc26RsPVNKgGjVNH_fSk)
 PATH = "/sdcard/Download/kushal/"
 file_id_cache = {} # Isse second time speed 100x ho jayegi
 
